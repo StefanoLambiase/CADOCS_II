ee1c12ae1c8ce350f762a3ab3290028885768303
Benedetto Scala <scala.benedetto@outlook.it>
2024-06-27 15:25:20 +0200

    Update ChatbotPage.css for loading message and format the message

--- frontend/src/pages/ChatbotPage.jsx
+++ frontend/src/pages/ChatbotPage.jsx
@@ -1,43 +1,58 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './ChatbotPage.css';
 
-
-
 const ChatbotPage = () => {
   const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [loadingMessage, setLoadingMessage] = useState('');
   const messagesEndRef = useRef(null);
 
-  // Funzione per gestire l'invio del messaggio
+  useEffect(() => {
+    // Messaggio di benvenuto del bot
+    const welcomeMessage = "Hello! 👋<br> I'm Cadocs, your virtual assistant. I'm here to help you identify 'community smells' and improve team collaboration. If you have any questions or need assistance, feel free to ask!<br><br> 😊Ciao! 👋<br> Sono Cadocs, il tuo assistente virtuale. Sono qui per aiutarti a individuare i 'community smells' e migliorare la collaborazione nel tuo team. Se hai domande o hai bisogno di assistenza, chiedimi pure!";
+    setLoading(true);
+
+    // Simulazione di un breve ritardo per il messaggio di benvenuto
+    setTimeout(() => {
+      setMessages([{ type: 'bot', text: welcomeMessage }]);
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    let loadingInterval;
+    if (loading) {
+      let catCount = 1;
+      loadingInterval = setInterval(() => {
+        setLoadingMessage('😺'.repeat(catCount));
+        catCount = catCount % 7 + 1; // Ciclo tra 1 e 7 gatti
+      }, 500);
+    } else {
+      setLoadingMessage('');
+      clearInterval(loadingInterval);
+    }
+    return () => clearInterval(loadingInterval);
+  }, [loading]);
+
   const handleSendMessage = async () => {
-    if (input.trim() === '') return;
+    if (userInput.trim() === '') return;
 
-    // Aggiungi il messaggio dell'utente alla lista dei messaggi
-    const userMessage = { text: input, sender: 'user' };
-    setMessages(prevMessages => [...prevMessages, userMessage]);
-    setInput('');
+    // Aggiungi il messaggio dell'utente
+    setMessages([...messages, { type: 'user', text: userInput }]);
+    setUserInput('');
+    setLoading(true);
 
+    // Effettua una richiesta al server
     try {
-      // Invia il messaggio all'endpoint specificato
-      const response = await fetch('http://127.0.0.1:5005/resolve_intent', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ message: input }),
-      });
-
-      const data = await response.json();
-      // Assicurati che `data.message` contenga il testo con \n
-      const botMessageText = processMessage(data|| JSON.stringify(data));
-
-      // Aggiungi il messaggio del bot alla lista dei messaggi
-      const botMessage = { text: botMessageText, sender: 'bot' };
-      setMessages(prevMessages => [...prevMessages, botMessage]);
+      const serverResponse = await fetchServerResponse(userInput);
+      // Aggiungi la risposta del server
+      setMessages([...messages, { type: 'user', text: userInput }, { type: 'bot', text: serverResponse }]);
     } catch (error) {
-      console.error('Errore nella richiesta:', error);
-      const botMessage = { text: 'Si è verificato un errore. Riprova più tardi.', sender: 'bot' };
-      setMessages(prevMessages => [...prevMessages, botMessage]);
+      console.error('Errore nella richiesta al server:', error);
+      setMessages([...messages, { type: 'user', text: userInput }, { type: 'bot', text: 'Errore nella risposta del server.' }]);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -48,7 +63,16 @@ const ChatbotPage = () => {
     }
     
     // Rileva i link nel testo e convertili in tag <a> HTML
-    text = text.replace(/(https?:\/\/[^\s]+)/g, '<a href="$1" target="_blank">$1</a>');
+    text = text.replace(/<(https?:\/\/[^\s]+)>/g, '<a href="$1" target="_blank">$1</a>');
+
+    // se c'è un messaggio contenuto in * * (asterischi), rendilo in grassetto
+    text = text.replace(/\*([^*]+)\*/g, '<b>$1</b>');
+
+    //se c'è un messaggio contenuto in _ _ (underscore), rendilo in corsivo
+    text = text.replace(/_([^_]+)_/g, '<i>$1</i>');
+
+    //Se c'è ----------------------------, sostituisci con una linea orizzontale nera con margin top di 10px
+    text = text.replace(/-{28,}/g, '<hr style="border-top: 1px solid black; margin-top: 10px;">');
 
     // Sostituisci \n con <br>
     return text.split('\n').join('<br>');
@@ -59,27 +83,56 @@ const ChatbotPage = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages]);
 
+  // Gestore di eventi per il tasto Invio
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSendMessage();
+    }
+  };
+
+  // Funzione per effettuare la richiesta al server
+  const fetchServerResponse = async (input) => {
+    const response = await fetch('http://127.0.0.1:5005/resolve_intent', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message: input }),
+    });
+    if (!response.ok) {
+      throw new Error('Errore nella richiesta al server');
+    }
+    const data = await response.json();
+    return processMessage(data); // Supponendo che il server restituisca un campo 'response'
+  };
+
   return (
     <div className="chatbot-page">
+      <div className="title">Chatbot</div>
       <div className="message-container">
-        {messages.map((msg, index) => (
-          <div key={index} className={`message ${msg.sender}`}>
-            {msg.sender === 'bot' ? (
-              <div dangerouslySetInnerHTML={{ __html: msg.text }} />
+        {messages.map((message, index) => (
+          <div key={index} className={`message ${message.type}`}>
+            {message.type === 'bot' ? (
+              <div dangerouslySetInnerHTML={{ __html: message.text }} />
             ) : (
-              <div>{msg.text}</div>
+              <div>{message.text}</div>
             )}
           </div>
         ))}
         <div ref={messagesEndRef}></div>
+        {loading && (
+          <div className="message bot loading-message">
+            {loadingMessage}
+          </div>
+        )}
       </div>
       <div className="user-input">
         <input
           type="text"
-          value={input}
-          onChange={e => setInput(e.target.value)}
-          onKeyPress={e => e.key === 'Enter' && handleSendMessage()}
-          placeholder="Chiedi a CADOCS."
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          onKeyPress={handleKeyPress} // Aggiungi gestore di eventi
+          placeholder="Scrivi un messaggio..."
         />
         <button onClick={handleSendMessage}>Invia</button>
       </div>
